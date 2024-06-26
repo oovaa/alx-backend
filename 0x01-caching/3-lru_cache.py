c19d3f31d581f3bd95cd9fbea55491bea9e2328d
oovaa <omarvx211@gmail.com>
2024-06-26 13:22:28 +0200

    adding docs at 26-13

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,19 +1,60 @@
 #!/usr/bin/env python3
+'''
+task 3 learning LRU the tough way
+'''
 
-from psutil import swap_memory
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
+    """
+    A class representing a Least Recently Used (LRU) Cache.
+
+    Inherits from the BaseCaching class.
+    """
+
     def __init__(self):
+        """
+        Initialize the LRUCache object.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         super().__init__()
         self.track = list()
 
     def handle_none_full_cache(self, key, item):
+        """
+        Handle the case when the cache is not full.
+
+        Adds the key and item to the cache.
+
+        Args:
+            key: The key to be added to the cache.
+            item: The item to be added to the cache.
+
+        Returns:
+            None
+        """
         self.track.append(key)
         self.cache_data[key] = item
 
     def handle_full_cache(self, key, item):
+        """
+        Handle the case when the cache is full.
+
+        Removes the least recently used item from the cache and adds the new key and item.
+
+        Args:
+            key: The key to be added to the cache.
+            item: The item to be added to the cache.
+
+        Returns:
+            None
+        """
         to_remove = self.track.pop(0)
         if to_remove in self.cache_data:
             self.cache_data.pop(to_remove)
@@ -24,10 +65,33 @@ class LRUCache(BaseCaching):
         self.cache_data[key] = item
 
     def swap(self, key):
+        """
+        Move the key to the end of the track list.
+
+        Args:
+            key: The key to be moved.
+
+        Returns:
+            None
+        """
         self.track.remove(key)
         self.track.append(key)
 
     def put(self, key, item):
+        """
+        Add a key-value pair to the cache.
+
+        If the key already exists in the cache, update its value.
+        If the cache is not full, add the key-value pair to the cache.
+        If the cache is full, remove the least recently used item and add the new key-value pair.
+
+        Args:
+            key: The key to be added to the cache.
+            item: The item to be added to the cache.
+
+        Returns:
+            None
+        """
         if item is None or key is None:
             return
         if key in self.cache_data:
@@ -39,6 +103,18 @@ class LRUCache(BaseCaching):
             self.handle_full_cache(key, item)
 
     def get(self, key):
+        """
+        Retrieve the value associated with the given key from the cache.
+
+        If the key exists in the cache, move it to the end of the track list and return its value.
+        If the key does not exist in the cache, return None.
+
+        Args:
+            key: The key to retrieve the value for.
+
+        Returns:
+            The value associated with the key, or None if the key does not exist in the cache.
+        """
         if key in self.cache_data and key is not None:
             if key in self.track:
                 self.swap(key)
