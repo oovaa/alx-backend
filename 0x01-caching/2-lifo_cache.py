73e8c3fe8fd68beb219129ae4d7a4a34767a9157
oovaa <omarvx211@gmail.com>
2024-06-26 11:54:36 +0200

    improve docs task 2 at 26-11

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -13,23 +13,48 @@ from base_caching import BaseCaching
 
 class LIFOCache(BaseCaching):
 
-    def __init__(self):
-        super().__init__()
-        self.id = 0
-        self.track = {}
+    class LIFOCache:
+        """
+        LIFOCache class represents a Last-In-First-Out (LIFO) cache.
+
+        Attributes:
+            id (int): The ID of the cache.
+            track (dict): A dictionary to track the cache items.
+        """
+
+        def __init__(self):
+            """
+            Initializes a new instance of the LIFOCache class.
+
+            The __init__ method is called when a new LIFOCache object is created.
+            It initializes the cache ID and the track dictionary.
+            """
+            super().__init__()
+            self.id = 0
+            self.track = {}
 
     def put(self, key, item):
-        if key is not None and item is not None:
-            if len(self.cache_data
-                   ) == self.MAX_ITEMS and key not in self.cache_data:
-                to_pop_key = self.track[max(self.track.keys())]
-                poped = self.cache_data.pop(to_pop_key)
-                print('DISCARD:', to_pop_key)
-                self.track.pop(max(self.track.keys()))
+            """
+            Adds a key-value pair to the cache.
 
-            self.track[self.id] = key
-            self.id += 1
-            self.cache_data[key] = item
+            Args:
+                key: The key to be added.
+                item: The value associated with the key.
+
+            Returns:
+                None
+            """
+            if key is not None and item is not None:
+                if len(self.cache_data
+                       ) == self.MAX_ITEMS and key not in self.cache_data:
+                    to_pop_key = self.track[max(self.track.keys())]
+                    poped = self.cache_data.pop(to_pop_key)
+                    print('DISCARD:', to_pop_key)
+                    self.track.pop(max(self.track.keys()))
+
+                self.track[self.id] = key
+                self.id += 1
+                self.cache_data[key] = item
 
     def get(self, key):
         """
